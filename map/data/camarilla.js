1f42b917888988ecfe12d90bca10002151302953
MontrealByNight <nottremere@gmail.com>
2026-04-15 13:25:42 +0100

    auto map reader

--- map/data/camarilla.js
+++ map/data/camarilla.js
@@ -1,139 +1,29 @@
 var data_camarilla = [
-  { 
-    "name": "Lakeshore Road", "lat": 45.4344, "lng": -73.9032, 
-    "type": "Camarilla",
-    "desc": "A scenic road that stretches along the northern edge of Lake Saint-Louis in Beaconsfield. The road is lined with well-kept homes, many with views of the lake, as well as small parks and green spaces. It’s a quiet, residential area, often used by locals for leisurely walks, cycling, and enjoying the waterfront scenery. The road provides easy access to the lake's edge, where the atmosphere is peaceful and relaxed.<br><b>Hunting:</b> Sandman (DC 4), Cleaver (DC 4)" 
-  },
-  { 
-    "name": "Beaconsfield Yacht Club", "lat": 45.4266, "lng": -73.8357, 
-    "type": "Camarilla",
-    "emoji": "🚢",
-    "desc": "A private club located along the shores of Lake Saint-Louis, offering facilities for sailing and other water-based activities. The club is a hub for local boating enthusiasts, with docks, storage areas for boats, and a clubhouse that offers both casual and formal spaces for members. The surrounding area includes scenic views of the lake and is a place for recreation and social gatherings, particularly during the warmer months.<br><br><b>🚢 The Undertow HQ</b><br><b>Domain:</b> ●●● <br><b>Chasse:</b> ●●● | <b>Lien:</b> ●●● | <b>Portillon:</b> ●●●<br><b>Hunting:</b> General Hunting DC 6" 
-  },
-  { 
-    "name": "Trinity Memorial Church", "lat": 45.4748, "lng": -73.6076, 
-    "type": "Camarilla",
-    "desc": "The historic and imposing 93-year-old gray limestone church on Sherbrooke Street West was dedicated as a memorial to soldiers who died in World War I. A stone slab at the entrance pays tribute to David Alwyn Forneri. After a fire in 2023, the interior was consumed in flames. It now belongs to a fund to repurpose the church. Gradually the building is being brought back to life, offering a safe space for people to gather. The sacristy is now a kitchen, however, it is intermittently closed for maintenance." 
-  },
-  { 
-    "name": "Villa Maria High School", "lat": 45.4795, "lng": -73.6163, 
-    "type": "Camarilla",
-    "desc": "A private, prestigious high school known for its strong academic reputation and traditional architecture. The campus includes a mix of historical buildings and modern facilities, with well-maintained gardens and athletic fields. It is a prominent educational institution in NDG, providing a range of academic and extracurricular programs for its students." 
-  },
-  { 
-    "name": "Our Lady of Grace", "lat": 45.4735, "lng": -73.6148, 
-    "type": "Camarilla",
-    "emoji": "⚜️",
-    "desc": "A modest stone parish church with a simple steeple rising above the surrounding neighborhood. Its exterior is light grey, with arched stained glass windows and a small set of steps leading to heavy wooden doors. Inside, the long nave is lined with wooden pews, leading to a raised altar beneath a vaulted ceiling.<br><br><b>⚜️ Maison Des Lys Violets HQ</b><br><b>Domain:</b> ●●● <br><b>Chasse:</b> ●●● | <b>Lien:</b> ●●● | <b>Portillon:</b> ●●●<br><b>Hunting:</b> Cleaver (DC 6)" 
-  },
-  { 
-    "name": "Devil's Hill", "lat": 45.4493, "lng": -73.6482, 
-    "type": "Camarilla",
-    "desc": "A prominent, elevated area known for its panoramic views of the surrounding neighborhood. The hill is surrounded by a mix of dense trees and open spaces, offering a natural, somewhat rugged terrain. It’s a quiet location, often frequented by those seeking solitude or a scenic place to relax.<br><b>Hunting:</b> Alleycat (DC 3), Trapdoor (DC 3), Pursuer (DC 3)" 
-  },
-  { 
-    "name": "Verdun Hospital", "lat": 45.4475, "lng": -73.5707, 
-    "type": "Camarilla",
-    "desc": "A large, modern healthcare facility located in the borough of LaSalle. The hospital complex includes multiple buildings, with a main structure that features a mix of contemporary and functional design. The atmosphere is generally busy, with a steady flow of patients, medical staff, and visitors throughout the day and night.<br><b>Hunting:</b> Grim Reaper (DC 2), Bagger (DC 2), Graverobber (DC 3)" 
-  },
-  { 
-    "name": "Beacon Hill Park", "lat": 45.4365, "lng": -73.8443, 
-    "type": "Camarilla", 
-    "desc": "A well-maintained public park offering a quiet retreat for the residents of Beaconsfield. The park features open green spaces, shaded areas under large trees, and walking paths. It's a popular spot for picnics, outdoor sports, and leisurely strolls. The park's calm atmosphere makes it a peaceful location for both relaxation and recreation." 
-  },
-  { 
-    "name": "Bistro le Bourbon", "lat": 45.4720, "lng": -73.6200, 
-    "type": "Camarilla", 
-    "desc": "A cozy and casual bistro located in NDG, known for its laid-back atmosphere and French-inspired menu. The interior is decorated with warm, rustic touches, including wooden tables, soft lighting, and vintage-style décor. It’s a popular spot for locals to enjoy a relaxed meal." 
-  },
-  { 
-    "name": "Montreal West Train Station", "lat": 45.4533, "lng": -73.6425, 
-    "type": "Camarilla", 
-    "desc": "A small, historic train station that serves as a central hub for commuters traveling in and out of Montreal West. The station is marked by classic architecture, with a waiting area that retains its vintage charm. It’s a functional space, with a small platform and nearby park for travelers to pass the time." 
-  },
-  { 
-    "name": "Boulevard Newman", "lat": 45.4313, "lng": -73.6353, 
-    "type": "Camarilla",
-    "desc": "A sprawling commercial artery running through the heart of LaSalle, dominated by strip malls, big-box retailers, and glowing fast-food chains. The avenue has a distinctly transient, suburban feel, defined by heavy car traffic and vast concrete parking lots. It is a hub of constant, impersonal motion.<br><b>Hunting:</b> Montero (DC 3), Extortionist (DC 3)" 
-  },
-  { 
-    "name": "Parc-nature du Bois-de-l'Île-Bizard", "lat": 45.5173, "lng": -73.8860, 
-    "type": "Camarilla",
-    "desc": "A large natural park located on the western tip of Île-Bizard, featuring expansive forested areas and winding trails. The park offers a variety of landscapes, including dense woods and open meadows. It is a popular spot for outdoor activities.<br><b>Hunting:</b> Farmer (DC 3)" 
-  },
-  { 
-    "name": "Île-Bizard Golf Club", "lat": 45.4865, "lng": -73.8860, 
-    "type": "Camarilla",
-    "desc": "An expansive golf course located on the southern edge of Île-Bizard. The course features well-maintained fairways, greens, and a clubhouse with views of the landscape. It is a quiet and scenic place, often frequented by golfers." 
-  },
-  { 
-    "name": "Faubourg de l'Île", "lat": 45.4950, "lng": -73.8750, 
-    "type": "Camarilla",
-    "desc": "A residential neighborhood known for its modern homes and suburban layout. The area features neatly maintained lawns, quiet streets, and cul-de-sacs. It is primarily a peaceful, residential area with a family-friendly atmosphere." 
-  },
-  { 
-    "name": "Carrefour d'Île-Bizard", "lat": 45.4905, "lng": -73.8720, 
-    "type": "Camarilla",
-    "desc": "A shopping complex that serves as the commercial hub of Île-Bizard, featuring a variety of small businesses, cafes, and grocery stores. The area is busy during the day with local residents shopping or running errands." 
-  },
-  { 
-    "name": "Trudeau Int. Airport", "lat": 45.4667, "lng": -73.7426, 
-    "type": "Camarilla",
-    "desc": "The airport terminal is a vast, modern structure with expansive glass walls, showcasing the constant flow of travelers through spacious, open concourses. The interior is bright and orderly, designed to support high traffic, with lounges, retail areas, and concourse seating.<br><b>Hunting:</b> Roadside Killer (DC 2)" 
-  },
-  { 
-    "name": "Dorval Train Station", "lat": 45.4452, "lng": -73.7415, 
-    "type": "Camarilla",
-    "desc": "Located near the airport, the train station is a functional, utilitarian space that connects travelers to the broader transit network. Its straightforward design emphasizes practical features like wide platforms, accessible stairways, and clear signage." 
-  },
-  { 
-    "name": "Côte-de-Liesse Industrial Park", "lat": 45.4850, "lng": -73.6950, 
-    "type": "Camarilla",
-    "desc": "This complex comprises large warehouses, distribution centers, and fenced storage facilities, with roads and loading zones designed to accommodate transport vehicles. The architecture is practical, characterized by wide, functional spaces." 
-  },
-  { 
-    "name": "La Source du Sexe", "lat": 45.4530, "lng": -73.7350, 
-    "type": "Camarilla",
-    "desc": "Located near the airport, La Source du Sexe is a well-known strip club with a bold neon-lit exterior. The venue is designed with a lounge-style interior, featuring a main stage, private seating sections, and a vibrant bar area.<br><b>Hunting:</b> Siren (DC 3)" 
-  },
-  { 
-    "name": "Cap-Saint-Jacques Nature Park", "lat": 45.4610, "lng": -73.9410, 
-    "type": "Camarilla",
-    "desc": "This vast, secluded park offers a range of landscapes—from dense forests to sandy beaches along Lake of Two Mountains. At night, the park takes on an eerie quiet, with only the rustle of leaves and distant water sounds breaking the silence." 
-  },
-  { 
-    "name": "Pierrefonds Cultural Centre", "lat": 45.4868, "lng": -73.8557, 
-    "type": "Camarilla",
-    "desc": "A community hub and event space, the Cultural Centre hosts everything from art exhibitions to workshops. With its contemporary architecture and glass façades, it stands as a beacon of local culture. At night, the building casts a soft glow over the nearby park." 
-  },
-  { 
-    "name": "Boulevard Gouin Ouest", "lat": 45.5090, "lng": -73.8340, 
-    "type": "Camarilla",
-    "desc": "This historic boulevard winds along the river, lined with heritage homes and quaint local shops. The architecture is a blend of rustic charm and elegance. By night, the street is quiet, with sparse streetlights illuminating the cobbled sidewalks." 
-  },
-  { 
-    "name": "S16 Art Gallery", "lat": 45.4828, "lng": -73.5898, 
-    "type": "Camarilla",
-    "desc": "A contemporary art gallery known for its innovative exhibitions that challenge traditional aesthetics. The gallery’s sleek, modern architecture stands out amidst Westmount’s historical surroundings, featuring an open layout with focused lighting." 
-  },
-  { 
-    "name": "Bibliothèque publique de Westmount", "lat": 45.4830, "lng": -73.5975, 
-    "type": "Camarilla",
-    "desc": "Westmount's public library, a blend of heritage charm and modernity, offers an inviting space with oak-paneled reading rooms, large arched windows, and cozy study areas. Its classic architecture makes it a popular spot for quiet reading." 
-  },
-  { 
-    "name": "Summit Woods", "lat": 45.4914, "lng": -73.6064, 
-    "type": "Camarilla",
-    "desc": "This wild, untamed urban forest provides a secluded escape with rugged dirt paths, dense undergrowth, and a heavy tree canopy. It features quiet, shaded trails and an elevated vantage point at the summit, making it ideal for absolute isolation." 
-  },
-  { 
-    "name": "Westmount Municipal Building", "lat": 45.4835, "lng": -73.5960, 
-    "type": "Camarilla",
-    "desc": "This stately building houses various administrative offices and serves as a civic hub for the area. With its grand stone architecture and classic style, it embodies the neighborhood's distinguished heritage, featuring historic woodwork and polished floors." 
-  },
-  { 
-    "name": "Westmount Greenhouses", "lat": 45.4805, "lng": -73.5970, 
-    "type": "Camarilla",
-    "desc": "Situated within Westmount Park, these historic greenhouses are home to a collection of exotic plants and seasonal blooms. Visitors can wander through glass-paned rooms filled with vibrant flora or sit in nearby shaded areas." 
-  }
+  { "name": "Lakeshore Road", "type": "Camarilla" },
+  { "name": "Beaconsfield Yacht Club", "type": "Camarilla", "emoji": "🚢" },
+  { "name": "Trinity Memorial Church", "type": "Camarilla" },
+  { "name": "Villa Maria High School", "type": "Camarilla" },
+  { "name": "Our Lady of Grace", "type": "Camarilla", "emoji": "⚜️" },
+  { "name": "Devil's Hill", "lat": "type": "Camarilla" },
+  { "name": "Verdun Hospital", "lat": "type": "Camarilla" },
+  { "name": "Beacon Hill Park", "lat": "type": "Camarilla" },
+  { "name": "Bistro le Bourbon", "lat": "type": "Camarilla" },
+  { "name": "Montreal West Train Station", "type": "Camarilla" },
+  { "name": "Boulevard Newman", "lat": "type": "Camarilla" },
+  { "name": "Parc-nature du Bois-de-l'Île-Bizard", "type": "Camarilla" },
+  { "name": "Île-Bizard Golf Club", "type": "Camarilla" },
+  { "name": "Faubourg de l'Île", "type": "Camarilla" },
+  { "name": "Carrefour d'Île-Bizard", "type": "Camarilla" },
+  { "name": "Trudeau Int. Airport", "type": "Camarilla" },
+  { "name": "Dorval Train Station", "type": "Camarilla" },
+  { "name": "Côte-de-Liesse Industrial Park", "type": "Camarilla" },
+  { "name": "La Source du Sexe", "lat": "type": "Camarilla" },
+  { "name": "Cap-Saint-Jacques Nature Park", "type": "Camarilla" },
+  { "name": "Pierrefonds Cultural Centre", "type": "Camarilla" },
+  { "name": "Boulevard Gouin Ouest", "type": "Camarilla" },
+  { "name": "S16 Art Gallery", "type": "Camarilla" },
+  { "name": "Bibliothèque publique de Westmount", "type": "Camarilla" },
+  { "name": "Summit Woods", "type": "Camarilla" },
+  { "name": "Westmount Municipal Building", "type": "Camarilla" },
+  { "name": "Westmount Greenhouses", "type": "Camarilla" }
 ];
